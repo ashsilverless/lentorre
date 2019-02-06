3cb25527ed947ff7c8510cb699ead3121d0fe33c
Ash - SIlverless <ash@silverless.co.uk>
2019-02-06 16:32:04 +0000

    Add code to drive hourly pages

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 ?>
 
 <style>
-    
+
 .loading {
     position: relative;
     display: grid;
@@ -18,12 +18,12 @@
     justify-content: center;
     text-align: center;
     background: hsl(29, 33%, 90%);
-}    
+}
 
 svg {
     width:10em;
     height: 10em;
-}    
+}
 
 @-webkit-keyframes svg_pulse {
 	 0% {
@@ -32,7 +32,7 @@ svg {
 		transform: scale(.25);
 		opacity: 0;
 	 }
-   
+
    25% {
      opacity: .5;
    }
@@ -61,27 +61,24 @@ svg {
 .svg-pulse .three {
   opacity: 0;
   animation-delay: 3s;
-} 
+}
 
 .svg-pulse .four {
   opacity: 0;
   animation-delay: 4.5s;
-}    
+}
 
 p {
     text-transform: uppercase;
     color: hsl(32, 6%, 50%);
     font-weight: 900;
     letter-spacing: 2px;
-}    
-    
+}
+
 </style>
 
-
-
-
 <div class="loading">
-  
+
   <svg version="1.1" class="svg-pulse" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
      viewBox="0 0 80 80" enable-background="new 0 0 80 80" xml:space="preserve">
     <circle class="pulse" fill="#E21A9E" stroke-miterlimit="10" cx="40" cy="40" r="14"/>
@@ -89,36 +86,54 @@ p {
     <circle class="pulse three" fill="#E21A9E" stroke-miterlimit="10" cx="40" cy="40" r="14"/>
     <circle class="pulse four" fill="#E21A9E" stroke-miterlimit="10" cx="40" cy="40" r="14"/>
 </svg>
- 
+
  <p>Loading</p>
-  
+
 </div>
 
 <script>
     var today = new Date().getHours();
 
-    if (today >= 0 && today < 8) {
-        window.location.replace("/nightime-description");
-    }
-    
-    if (today >= 8 && today < 12) {
-        window.location.replace("/morning-description");
-    }
-    
-    if (today >= 12 && today < 15) {
-        window.location.replace("/lunchtime-description");
-    }
-    
-    if (today >= 15 && today < 18) {
-        window.location.replace("/afternoon-description");
-    }
-    
-    if (today >= 18 && today < 22) {
-        window.location.replace("/evening-description");
-    }  
-    
-    if (today >= 22 && today < 23) {
-        window.location.replace("/night-time-description");
-    }
-  </script>
+    if (today >= 0 && today < 3) { window.location.replace("/11pm-3am");}
 
+    if (today >= 4 && today < 5) { window.location.replace("/4am");}
+
+    if (today >= 5 && today < 6) { window.location.replace("/5am");}
+
+    if (today >= 6 && today < 7) { window.location.replace("/6am");}
+
+    if (today >= 7 && today < 8) { window.location.replace("/7am");}
+
+    if (today >= 8 && today < 9) { window.location.replace("/8am");}
+
+    if (today >= 9 && today < 10) { window.location.replace("/9am");}
+
+    if (today >= 10 && today < 11) { window.location.replace("/10am");}
+
+    if (today >= 11 && today < 12) { window.location.replace("/11am");}
+
+    if (today >= 12 && today < 13) { window.location.replace("/12pm");}
+
+    if (today >= 13 && today < 14) { window.location.replace("/1pm");}
+
+    if (today >= 14 && today < 15) { window.location.replace("/2pm");}
+
+    if (today >= 15 && today < 16) { window.location.replace("/3pm");}
+
+    if (today >= 16 && today < 17) { window.location.replace("/4pm");}
+
+    if (today >= 17 && today < 18) { window.location.replace("/5pm");}
+
+    if (today >= 18 && today < 19) { window.location.replace("/6pm");}
+
+    if (today >= 19 && today < 20) { window.location.replace("/7pm");}
+
+    if (today >= 20 && today < 21) { window.location.replace("/8pm");}
+
+    if (today >= 21 && today < 22) { window.location.replace("/9pm");}
+
+    if (today >= 22 && today < 23) { window.location.replace("/10pm");}
+
+    if (today >= 23 && today < 24) { window.location.replace("/11pm-3am");}
+
+  </script>
--- page-templates/home.php
+++ page-templates/home.php
@@ -10,161 +10,161 @@ get_header();?>
  <?php the_field('heading');?>   -->
 
 <div class="hero">
-    
+
     <div class="hero__upper">
     <?php
         $frontImg = get_field('hero_front');
         $midImg = get_field('hero_mid');
-        $rearImg = get_field('hero_rear');     
-    ?>   
+        $rearImg = get_field('hero_rear');
+    ?>
 
-        <div class="rear-image" style="background-image: url(<?php echo $rearImg['url']; ?>);"></div>    
-        
-        <div class="mid-image" style="background-image: url(<?php echo $midImg['url']; ?>);"></div>        
-        
-        <div class="front-image" style="background-image: url(<?php echo $frontImg['url']; ?>);"></div>        
+        <div class="rear-image" style="background-image: url(<?php echo $rearImg['url']; ?>);"></div>
+
+        <div class="mid-image" style="background-image: url(<?php echo $midImg['url']; ?>);"></div>
+
+        <div class="front-image" style="background-image: url(<?php echo $frontImg['url']; ?>);"></div>
 
         <div class="headline">
-            
+
             <div class="container">
-            
+
                 <div class="row">
-                
-                    <div class="col-6">                
-                
+
+                    <div class="col-6">
+
                         <h3 class="heading-secondary heading-secondary__sm"><?php the_field('sub_heading');?></h3>
-                           
-                        <h2 class="heading-primary heading-primary__xl heading-primary__bold"><?php the_field('heading');?></h2>      
+
+                        <h2 class="heading-primary heading-primary__xl heading-primary__bold"><?php the_field('heading');?></h2>
 
                     </div>
-                         
-                </div><!--r-->         
-                                 
-            </div><!--c-->                     
-                                 
+
+                </div><!--r-->
+
+            </div><!--c-->
+
         </div>
 
-    </div>    
-    
+    </div>
+
     <div id="hero-lower" class="hero__lower">
-    
+
         <div class="timeline">
-        
-            <div class="hour" id='hour-1'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">1am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>   
-            <div class="hour" id='hour-2'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">2am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-3'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">3am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-4'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">4am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-5'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">5am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-6'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">6am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>        
-            <div class="hour" id='hour-7'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">7am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-8'><a href="/morning-description" class="link"></a><div class="time-wrapper" value="yeppp"><span class="hour-mark">8am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-9'><a href="/morning-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">9am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-10'><a href="/morning-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">10am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-11'><a href="/morning-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">11am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-12'><a href="/lunchtime-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">12pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>       
-            <div class="hour" id='hour-13'><a href="/lunchtime-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">1pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>      
-            <div class="hour" id='hour-14'><a href="/lunchtime-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">2pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-15'><a href="/afternoon-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">3pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-16'><a href="/afternoon-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">4pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-17'><a href="/afternoon-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">5pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-18'><a href="/evening-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">6pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-19'><a href="/evening-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">7pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-20'><a href="/evening-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">8pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-21'><a href="/evening-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">9pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-22'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">10pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-23'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">11pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-            <div class="hour" id='hour-00'><a href="/night-time-description" class="link"></a><div class="time-wrapper"><span class="hour-mark">Midnight</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>  
-        
+
+            <div class="hour" id='hour-1'><a href="/11pm-3am" class="link"></a><div class="time-wrapper"><span class="hour-mark">1am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-2'><a href="/11pm-3am" class="link"></a><div class="time-wrapper"><span class="hour-mark">2am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-3'><a href="/11pm-3am" class="link"></a><div class="time-wrapper"><span class="hour-mark">3am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-4'><a href="/4am" class="link"></a><div class="time-wrapper"><span class="hour-mark">4am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-5'><a href="/5am" class="link"></a><div class="time-wrapper"><span class="hour-mark">5am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-6'><a href="/6am" class="link"></a><div class="time-wrapper"><span class="hour-mark">6am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-7'><a href="/7am" class="link"></a><div class="time-wrapper"><span class="hour-mark">7am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-8'><a href="/8am" class="link"></a><div class="time-wrapper" value="yeppp"><span class="hour-mark">8am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-9'><a href="/9am" class="link"></a><div class="time-wrapper"><span class="hour-mark">9am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-10'><a href="/10am" class="link"></a><div class="time-wrapper"><span class="hour-mark">10am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-11'><a href="/11am" class="link"></a><div class="time-wrapper"><span class="hour-mark">11am</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-12'><a href="/12pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">12pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-13'><a href="/1pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">1pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-14'><a href="/2pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">2pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-15'><a href="/3pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">3pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-16'><a href="/4pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">4pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-17'><a href="/5pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">5pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-18'><a href="/6pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">6pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-19'><a href="/7pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">7pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-20'><a href="/8pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">8pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-21'><a href="/9pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">9pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-22'><a href="/10pm" class="link"></a><div class="time-wrapper"><span class="hour-mark">10pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-23'><a href="/11pm-3am" class="link"></a><div class="time-wrapper"><span class="hour-mark">11pm</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+            <div class="hour" id='hour-00'><a href="/11pm-3am" class="link"></a><div class="time-wrapper"><span class="hour-mark">Midnight</span><span></span><?php get_template_part( 'template-parts/svg', 'clock' );?></div></div>
+
         </div>
-    
+
         <div class="container">
-            
+
             <div class="row">
-                
+
                 <div class="col-6 vertical-center">
-                    
+
                     <a href="" class="button button__prompt button__dark-hover">
                         <span>Book Now</span>
                     </a>
-                    
+
                 </div>
-                
+
                 <div class="col-6 vertical-center">
-                    
+
                     <p><?php the_field('teaser');?></p>
-                    
+
                     <div class="button button__ghost button__fullwidth button__prompt button__prompt__down reveal-context">
                         <span>Learn More</span>
-                        
+
                     </div>
-                                
-                </div>        
-                
+
+                </div>
+
             </div><!--r-->
-            
+
         </div><!--c-->
-        
-    </div>    
-    
+
+    </div>
+
 </div>
 
         <div class="context-panel">
-            
+
             <div class="container">
-                
-                <div class="row">    
+
+                <div class="row">
 
                     <div class="col-12 vertical-center">
 
 <div class="owl-carousel owl-theme image-carousel">
-                 
+
                         	<?php $images = get_field('image_carousel');
-                            	
-                        	foreach ($images as $image):?> 		
-                   	
-                       <div class="item">		
+
+                        	foreach ($images as $image):?>
+
+                       <div class="item">
                         			<img class="slide" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
                        </div>
-                        		 
+
                         	<?php endforeach;?>
-                        	
+
                         </div>
 
-                    </div>    
-                    
+                    </div>
+
                     <div class="col-6">
 
                         <div class="lodge-now">
-                        
+
                             <p class="heading-secondary heading-secondary__xs">
-                                
+
                                 <span id="timeHolder"></span> at Lentorre Lodge
-                            
-                            </p>                           
-                            
-                            <p class="heading-primary heading-primary__md"><?php the_field('current_activity');?></p>                            
-                        
+
+                            </p>
+
+                            <p class="heading-primary heading-primary__md"><?php the_field('current_activity');?></p>
+
                         </div>
-                        
-                    </div>               
+
+                    </div>
 
                     <div class="col-6 vertical-center">
-                        
-                        <?php the_field('text_content');?>
-                        
-                        <a href="/activities" class="button button__prompt button__dark-hover"><span>Learn More About Activities</span></a>
-                        
-                    </div>               
-                    
-            
-                </div><!--r-->     
-                          
-            </div><!--c-->
-            
-            
 
-        </div> 
- 
-<?php get_template_part( 'page-templates/home', 'lower' );?> 
+                        <?php the_field('text_content');?>
+
+                        <a href="/activities" class="button button__prompt button__dark-hover"><span>Learn More About Activities</span></a>
+
+                    </div>
+
+
+                </div><!--r-->
+
+            </div><!--c-->
+
+
+
+        </div>
+
+<?php get_template_part( 'page-templates/home', 'lower' );?>
 
 <?php get_footer();?>
